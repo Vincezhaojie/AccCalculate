f9815f7b3876fda48d45d06e5fd65156e0253d58
Zhao Jie <Vince_zhaojie@yahoo.com>
2020-05-18 22:30:04 +0800

    option to select filed or cloud

--- Acc4Calculate.py
+++ Acc4Calculate.py
@@ -2,20 +2,22 @@ import numpy as np
 import pandas as pd
 import os
 
-def readDataSet():
-    cur_path = os.getcwd()
-    dataset_path = cur_path + '\超短期单点数据集'
-    files = os.listdir(dataset_path)
-    print(files)
-    if not files:
-        print('There is no Dataset in ...\超短期单点数据集, please check it!')
-        return
-    df_all = pd.DataFrame()
-    for file in files:
-        df = pd.read_excel(dataset_path + '\\' + file)
-        df_all = pd.concat([df_all, df])
-    print(df_all)
-    pass
+def readDataSet(field=True):
+    if field == True:
+        cur_path = os.getcwd()
+        dataset_path = cur_path + '\现场数据\超短期单点数据集'
+        files = os.listdir(dataset_path)
+        print(files)
+        if not files:
+            print('There is no Dataset in ...\现场数据\超短期单点数据集, please check it!')
+            return
+        df_all = pd.DataFrame()
+        for file in files:
+            df = pd.read_excel(dataset_path + '\\' + file)
+            df_all = pd.concat([df_all, df])
+    else:
+
+
 
 def calc():
     pass
